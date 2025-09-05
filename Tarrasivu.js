336c8520f7ff458aa172d24bd29b7e123498ed6f
Flafiee <whitenremi@gmail.com>
2025-09-05 10:20:20 +0300

    fix

--- Tarrasivu.js
+++ Tarrasivu.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 // sivulta Medium
 function AvaaLisää(elementId, visible = true) {
     const element = document.getElementById(elementId);
     element.style.visibility = visible ? 'visible' : 'hidden';
 }
-=======
- 
 
- 
->>>>>>> 382557914579c2beedaa1760c5996425ce6c7a3f
