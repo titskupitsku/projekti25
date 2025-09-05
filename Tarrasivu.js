a044ef96cd1643c0b40e88d6d3cd9be470f3e168
Flafiee <whitenremi@gmail.com>
2025-09-05 10:50:49 +0300

    Nappi fully works as intended :O.

--- Tarrasivu.js
+++ Tarrasivu.js
@@ -1,7 +1,26 @@
+const Button = document.getElementById("AvaaLisää");
+const Näytä = document.getElementById("Avaa");
 
-// sivulta Medium (base structure)
-function AvaaLisää(visible = true) {
-    const element = document.getElementById("Avaa")
-    element.style.visibility = visible ? 'visible' : 'hidden';
-}
+Button.addEventListener("click", event => {
+   
+    //Display
+    if(Näytä.style.display === "none"){
+        Näytä.style.display = "block";
+        Button.textContent = "Hide";
+    }
+    else{
+        Näytä.style.display = "none";
+        Button.textContent = "Show";
+    }
+   
+   //Visibility
+    if(Näytä.style.visibility === "hidden"){
+        Näytä.style.visibility = "visible";
+        Button.textContent = "Hide";
+    }
+    else{
+        Näytä.style.visibility = "hidden";
+        Button.textContent = "Show";
+    }
+});
 
