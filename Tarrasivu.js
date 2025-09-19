2ca00048fdaee804be6502c81318e2327dc9d310
Flafiee <whitenremi@gmail.com>
2025-09-19 10:20:56 +0300

    revert js lyhennys tarrasivulla

--- Tarrasivu.js
+++ Tarrasivu.js
@@ -4,11 +4,22 @@ const Näytä = document.getElementById("Avaa");
 Button.addEventListener("click", event => {
    
     //Display thingy
-    if (Näytä.style.display === "none" || Näytä.style.display === "") {
+    if(Näytä.style.display === "none"){
         Näytä.style.display = "grid";
         Button.textContent = "Hide";
-    } else {
+    }
+    else{
         Näytä.style.display = "none";
+        Button.textContent = "Show";
+    }
+   
+   //Visibility asiat
+    if(Näytä.style.visibility === "hidden"){
+        Näytä.style.visibility = "visible";
+        Button.textContent = "Hide";
+    }
+    else{
+        Näytä.style.visibility = "hidden";
         Button.textContent = "Show";
     }
 });
