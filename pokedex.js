ce49af0ff36aa48b3387bfde0656525b65cac0fd
titi <tm4262@edu.turku.fi>
2025-09-19 12:24:49 +0300

    pokemon animaatio + linkkifix

--- pokedex.js
+++ pokedex.js
@@ -1,42 +1,34 @@
 const pokemonData = {
     "0001": {
       name: "Bulbasaur",
+      page: "pokemonsivut/0001bulbasaur.html",
       image: "pokedex-kuvat/pokemon_icon_001_00.png",
       type: "Poison"
     },
     "0002": {
       name: "Ivysaur",
-      image: "pokedex-kuvat/0002ivysaur.png",
+      page: "pokemonsivut/0002venusaur.html",
+      image: "pokedex-kuvat/pokemon_icon_002_00.png",
       type: "Grass/Poison"
     },
     "0003": {
       name: "Venusaur",
-      image: "pokedex-kuvat/0003venusaur.png",
-      type: "Poison-grass"
+      page: "pokemonsivut/0003ivysaur.html",
+      image: "pokedex-kuvat/pokemon_icon_003_00.png",
+      type: "Grass/Poison"
     }
   };
 const pokeItems = document.querySelectorAll('.poke-show');
-
-
-  // Add click event to each .poke-show
   pokeItems.forEach(item => {
     item.addEventListener('click', () => {
       pokeItems.forEach(el => el.classList.remove('active'));
-
-      // Add 'active' to clicked one
       item.classList.add('active');
-
 
       const id = item.id || item.textContent.match(/#(\d{4})/)[1];
       const data = pokemonData[id];
 
-      // Update name
-      document.getElementById('poke-name').innerHTML = `<p>${data.name}</p>`;
-
-      // Update image
-      document.getElementById('poke-preview').innerHTML = `<img src="${data.image}" alt="${data.name}" />`;
-
-      // Update type
+      document.getElementById('poke-name').innerHTML = `<p><a href="${data.page}"class="poke-link">${data.name}</a></p>`;
+      document.getElementById('poke-preview').innerHTML = `<img src="${data.image}" alt="${data.name}"/>`;
       document.getElementById('poke-type').innerHTML = `<p>${data.type}</p>`;
     });
   });
